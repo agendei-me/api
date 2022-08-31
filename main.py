ef782528e76aa44e7d7ae8b45f1cba6684cbf457
imcaiofabio <caio.fabio91@gmail.com>
2022-08-30 23:00:46 -0300

    Customer CRUD
    - [x] created post method

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Request
 import uvicorn
-from app.routes import agent
+from app.routes import agent, customer
 
 
 app = FastAPI()
 app.include_router(agent.router)
+app.include_router(customer.router)
 
 
 if __name__ == '__main__':
