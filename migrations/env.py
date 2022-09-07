ba692275d60d260a667316ac3f4f5aa26e9545ee
imcaiofabio <caio.fabio91@gmail.com>
2022-09-07 12:12:54 -0300

    CI/CD

--- migrations/env.py
+++ migrations/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -39,7 +40,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = os.getenv("DB_URL")  #config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
