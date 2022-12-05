1d67f8db121e865a764bef4f9c4963400eb6a193
imcaiofabio <caio.fabio91@gmail.com>
2022-12-05 18:38:11 -0300

    [feature] - Adding layer to save on database

--- app/models/appointment.py
+++ app/models/appointment.py
@@ -16,6 +16,7 @@ class Appointment(SQLModel, table=True):
     state: States = Field(default=States.scheduled, max_length=20)
     observation: str = Field(default=None, max_length=255)
 
+    calendar_id: str = Field(nullable=False)
     event_id: str = Field(nullable=False)
     client_id: str = Field(nullable=False, foreign_key="clients.id")
     created_at: datetime = Field(nullable=False, default=datetime.now())
